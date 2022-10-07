30209810b805d0bbdba7587d05c5a78b11e6e261
Jesier <jesiersantiago@gmail.com>
2022-10-07 09:50:58 -0500

    pushin

---
+++ main.js
@@ -0,0 +1,8 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ];
+const toFireInKiln = []
+
+for (let i = 0; i < 4; i++ ) {
+    toFireInKiln[i] = `${clay[i]} coffee mug`
+};
+
+console.log(toFireInKiln);
